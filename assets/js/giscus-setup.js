e32b71ff4d14b0e56981250a88652ee1e90a29e6
vbipba <vb.ipba@gmail.com>
2026-03-31 19:20:33 +0000

    Deploying to gh-pages from @ vbipba/Vaibhav_Bajpai.github.io@27a0732618d9fd734b1288b9ef828b6ba030237e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "vbipba/Vaibhav_Bajpai",
+    "data-repo": "vbipba/Vaibhav_Bajpai.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
